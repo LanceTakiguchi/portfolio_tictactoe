fd6edea7a64f8357a66b3e998fe45c955f82e415
Collette-Tamez <cttmz4@gmail.com>
2016-09-15 10:00:11 -0700

    creating player selection logic

--- additional_script.js
+++ additional_script.js
@@ -1,14 +1,33 @@
 //TODO: FUNCTION TO RESET VARIABLE
 //TODO: FUNCTION TO APPLY/TOGGLE CLASSES?
-
+var hasPlayer = false;
 $(document).ready(function () {
     applyClickHandlers();
 });
 
 function applyClickHandlers() {
     $("button").click(selectedButton);
+    $("#co1").click(selectGameTiles);
+    $("#co2").click(selectGameTiles);
+    $("#co3").click(selectGameTiles);
+    $("#co4").click(selectGameTiles);
+    $("#startGame").click(startTheGame);
 }
 
 function selectedButton() {
-    $("button").addClass("selected");
+    $(this).toggleClass("selected");
+}
+
+function selectGameTiles() {
+    if(hasPlayer){
+        $(this).appendTo("#team2_display");
+    } else {
+        $(this).appendTo("#team1_display");
+    }
+    hasPlayer = true;
+    ///$(this).appendTo("#team1_display").toggleClass("selected");
+
+}
+function startTheGame() {
+    $("#setUpMenu").addClass("hide");
 }
