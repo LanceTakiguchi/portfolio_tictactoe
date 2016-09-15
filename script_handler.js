be4507cf1f47af7eed528fbbf20d230ef4826a08
Dave Weizenegger <Dave Weizenegger>
2016-09-15 08:35:55 -0700

    damn the white rabbit

--- script_handler.js
+++ script_handler.js
@@ -6,30 +6,37 @@ var rowEndParam = "</tr>";
 var tdStartEndParam = "<td></td>"
 
 function makeBoard (boardSize) {
-/*dave*/
-    var boardRow = rowStartParam;
+    /*dave*/
+    // var boardRow = rowStartParam;
+    // for (var i = 0; i < boardSize; i++) {
+    //     boardRow += tdStartEndParam;
+    // }
+    // boardRow += rowEndParam;
+    // for (var j = 0; j < boardSize; j++) {
+    //     $("#board > tbody").append(boardRow);
+    // }
+    /*end dave*/
+    /*dan*/
+
+
+    $("#board > tbody").html('');
+    var cell_size_percent = 100 / boardSize + '%';
     for (var i = 0; i < boardSize; i++) {
-        boardRow += tdStartEndParam;
-    }
-    boardRow += rowEndParam;
-    for (var j = 0; j < boardSize; j++) {
+        var boardRow = $("<tr>");
+        for (var j = 0; j < boardSize; j++) {
+            var boardCell = $("<td>", {
+                id: 'cell_' + i + '_' + j,
+                class: 'board_'+boardSize
+            }).css({
+                height: cell_size_percent,
+                width: cell_size_percent
+            });
+            boardRow.append(boardCell);
+        }
         $("#board > tbody").append(boardRow);
     }
-/*end dave*/
-/*dan*/
 
-/*end dan*/
-    switch(boardSize) {
-        case 3:
-            $('td').removeClass('nine').removeClass('twenty').addClass('three');
-            break;
-        case 9:
-            $('td').removeClass('three').removeClass('twenty').addClass('nine');
-            break;
-        case 20:
-            $('td').removeClass('three').removeClass('nine').addClass('twenty');
-            break;
-    }
+    /*end dan*/
+
+
 }
-
-
